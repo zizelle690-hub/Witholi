3ea709a0fb668847f0318e1fb8cb4dd5f1cf8e41
zizelle690 <zizelle690@gmail.com>
2026-05-30 00:46:59 +0100

    update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://witholi.netlify.app',,
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
